0263da1b784da9dd9e770a91d66bc752e6e75df8
Seth Faerber <sethfaerber@yahoo.com>
2022-01-26 21:24:18 -0600

    Fixed responsive carousel, corrupted node modules, and working on media query for carousel

--- src/components/Collections.js
+++ src/components/Collections.js
@@ -1,12 +1,38 @@
-import React from 'react';
 
-function Collections() {
-    return (
-        <React.Fragment>
-            <h1 style={{padding: "250px"}}>Custom Collections on Their Way!</h1>
-        </React.Fragment>
-    )
+import React, { Component } from 'react';
+
+class Collections extends React.Component {
+    constructor(props) {
+        super(props)
+        this.state = { matches: window.matchMedia("(min-width: 768px)").matches };
+    }
+    
+    componentDidMount() {
+        const handler = e => this.setState({matches: e.matches});
+        window.matchMedia("(min-width: 768px)").addEventListener('change', handler);
+    }
+    render() {
+        console.log(this.state.matches)
+        return (
+            <div >
+                {this.state.matches && (<h1>Big Screen</h1>)}
+                {!this.state.matches && (<h3>Small Screen</h3>)}
+            </div>
+            
+        );
+    }
 }
 
+export default Collections;
+// import React from 'react';
 
-export default Collections
+// function Collections() {
+//     return (
+//         <React.Fragment>
+//             <h1 style={{padding: "250px"}}>Custom Collections on Their Way!</h1>
+//         </React.Fragment>
+//     )
+// }
+
+
+// export default Collections
--- src/components/homeComponents/CarouselDebug.js
+++ src/components/homeComponents/CarouselDebug.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { Component, useState } from 'react';
 import {
     Carousel,
     CarouselItem,
@@ -7,73 +7,96 @@ import {
     CarouselCaption
 } from 'reactstrap';
 
-const items = [
-    {
-        src: 'assets/images/manet-l.jpg',
-        altText: 'Slide 1',
-        caption: 'Slide 1'
-    },
-    {
-        src: 'assets/images/turner-l.jpg',
-        altText: 'Slide 2',
-        caption: 'Slide 2'
-    },
-    {
-        src: 'assets/images/twombly-l.jpg',
-        altText: 'Slide 3',
-        caption: 'Slide 3',
-        style: 'dark'
+
+class Collections extends Component {
+    constructor(props) {
+        super(props)
+        this.state = { 
+            matches: window.matchMedia("(min-width: 768px)").matches 
+        };
     }
-];
-
-const CarouselDebug = (props) => {
-    const [activeIndex, setActiveIndex] = useState(0);
-    const [animating, setAnimating] = useState(false);
-
-    const next = () => {
-        if (animating) return;
-        const nextIndex = activeIndex === items.length - 1 ? 0 : activeIndex + 1;
-        setActiveIndex(nextIndex);
+    
+    componentDidMount() {
+        const handler = e => this.setState({matches: e.matches});
+        window.matchMedia("(min-width: 768px)").addEventListener('change', handler);
     }
-
-    const previous = () => {
-        if (animating) return;
-        const nextIndex = activeIndex === 0 ? items.length - 1 : activeIndex - 1;
-        setActiveIndex(nextIndex);
+    render() {
+        return (
+            <div >
+                {this.state.matches && (CarouselDebug)}
+                {!this.state.matches && (<h3>Small Screen</h3>)}
+            </div>
+        );
     }
-
-    const goToIndex = (newIndex) => {
-        if (animating) return;
-        setActiveIndex(newIndex);
-    }
-
-    const slides = items.map((item) => {
-        //TODO: This if/else statement is a heavy-handed way of turning the text dark on slide 3. Figure something else out.
-        if(item.style === 'dark'){
-            return (
-            <CarouselItem
-                onExiting={() => setAnimating(true)}
-                onExited={() => setAnimating(false)}
-                key={item.src}
-            >
-                <img src={item.src} alt={item.altText} />
+}
+    
+    const items = [
+        {
+            src: 'assets/images/manet-l.jpg',
+            altText: 'Slide 1',
+            caption: 'Slide 1'
+        },
+        {
+            src: 'assets/images/turner-l.jpg',
+            altText: 'Slide 2',
+            caption: 'Slide 2'
+        },
+        {
+            src: 'assets/images/twombly-l.jpg',
+            altText: 'Slide 3',
+            caption: 'Slide 3',
+            style: 'dark'
+        }
+    ];
+    
+    const CarouselDebug = (props) => {
+        const [activeIndex, setActiveIndex] = useState(0);
+        const [animating, setAnimating] = useState(false);
+        
+        const next = () => {
+            if (animating) return;
+            const nextIndex = activeIndex === items.length - 1 ? 0 : activeIndex + 1;
+            setActiveIndex(nextIndex);
+        }
+        
+        const previous = () => {
+            if (animating) return;
+            const nextIndex = activeIndex === 0 ? items.length - 1 : activeIndex - 1;
+            setActiveIndex(nextIndex);
+        }
+        
+        const goToIndex = (newIndex) => {
+            if (animating) return;
+            setActiveIndex(newIndex);
+        }
+        
+        const slides = items.map((item) => {
+            //TODO: This if/else statement is a heavy-handed way of turning the text dark on slide 3. Figure something else out.
+            if(item.style === 'dark'){
+                return (
+                    <CarouselItem
+                    onExiting={() => setAnimating(true)}
+                    onExited={() => setAnimating(false)}
+                    key={item.src}
+                    >
+                <img className="img-fluid" src={item.src} alt={item.altText}/>
                 <CarouselCaption className="text-dark" captionText={item.caption} captionHeader={item.caption} />
             </CarouselItem>
             )
         } else {
             return (
                 <CarouselItem
-                    onExiting={() => setAnimating(true)}
-                    onExited={() => setAnimating(false)}
-                    key={item.src}
+                onExiting={() => setAnimating(true)}
+                onExited={() => setAnimating(false)}
+                key={item.src}
                 >
-                <img src={item.src} alt={item.altText} />
+                <img className="img-fluid" src={item.src} alt={item.altText} />
                 <CarouselCaption captionText={item.caption} captionHeader={item.caption} />
                 </CarouselItem>
             )
         };
     });
-
+    
     return (
         <Carousel
             activeIndex={activeIndex}
@@ -87,5 +110,6 @@ const CarouselDebug = (props) => {
         </Carousel>
     );
 }
+
 
 export default CarouselDebug;
